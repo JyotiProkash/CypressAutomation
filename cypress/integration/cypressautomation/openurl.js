7d274192ac814a554ba5a5ecf177eae41ea934cb
JyotiProkash <cse0718@gmail.com>
2021-08-17 00:30:19 +0600

    first commit

---
+++ cypress/integration/cypressautomation/openurl.js
@@ -0,0 +1,6 @@
+describe('Open Url',()=>{
+    it('Test open url', ()=>{
+        cy.visit('https://demoqa.com/automation-practice-form');
+        cy.title().should('include','ToolsQA')
+    })
+})
